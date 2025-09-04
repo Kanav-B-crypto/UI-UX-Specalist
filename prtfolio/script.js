0ceab3c6c00795aafeeea214ecfedbfe9b36de01
Kanav-B-crypto <kanavb86@gmail.com>
2025-09-04 14:18:52 +0530

    Add files via upload

---
+++ prtfolio/script.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+  });
+});
+
+
+const sections = document.querySelectorAll(".s");
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.1 });
+sections.forEach(section => observer.observe(section));
+
+
+
+
